9fd090aa293a3ec41f5aadee2cf39f1336d189b4
Thuhiennguyen <31833651+Thuhiennguyen@users.noreply.github.com>
2017-10-14 18:57:21 +0700

    Commit lần 2
    Lần 2

---
+++ MyPhotos/MyPhotoAlbum/PhotoAlbum.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mainning.MyPhotoAlbum
+{
+   public class PhotoAlbum : Collection<Photograph>, IDisposable
+    {
+        private bool _hasChanged = false;
+        public bool HasChanged
+        {
+            get
+            {
+                if (_hasChanged) return true;
+                foreach (Photograph p in this)
+                    if (p.HasChanged) return true;
+                return false;
+            }
+            set
+            {
+                _hasChanged = value;
+                if (value == false)
+                    foreach (Photograph p in this)
+                        p.HasChanged = false;
+            }
+        }
+        public Photograph Add(string filename)
+        {
+            Photograph p = new Photograph(filename);
+            base.Add(p);
+            return p;
+        }
+        protected override void ClearItems()
+        {
+            if (Count > 0)
+            {
+                Dispose();
+                base.ClearItems();
+                HasChanged = true;
+            }
+        }
+
+        protected override void InsertItem(int index, Photograph item)
+        {
+            base.InsertItem(index, item);
+            HasChanged = true;
+        }
+
+        protected override void RemoveItem(int index)
+        {
+            Items[index].Dispose();
+            base.RemoveItem(index);
+            HasChanged = true;
+        }
+
+        protected override void SetItem(int index, Photograph item)
+        {
+            base.SetItem(index, item);
+            HasChanged = true;
+        }
+
+        public void Dispose()
+        {
+            foreach (Photograph p in this)
+                p.Dispose();
+        }
+
+    }
+}
---
+++ MyPhotos/MyPhotoAlbum/Photograph.cs
@@ -0,0 +1,135 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+
+namespace Mainning.MyPhotoAlbum
+{
+    /// <summary>
+    /// The photograph class represents a photographic
+    /// image stored in a file system
+    /// </summary>
+   public class Photograph : IDisposable
+    {
+        private string _fileName;
+        public string FileName
+        {
+            get { return this._fileName; }
+        }
+
+        private Bitmap _bitmap;
+        public Bitmap Image
+        {
+            get
+            {
+                if (_bitmap == null)
+                    _bitmap = new Bitmap(_fileName);
+                return _bitmap;
+            }
+        }
+
+        private string _caption = "";
+        public string Caption
+        {
+            get { return _caption; }
+            set
+            {
+                if (_caption != value)
+                {
+                    _caption = value;
+                    HasChanged = true;
+                }
+            }
+        }
+        private string _photographer = "";
+        public string Photographer
+        {
+            get { return _photographer; }
+            set
+            {
+                if (_photographer != value)
+                {
+                    _photographer = value;
+                    HasChanged = true;
+                }
+            }
+        }
+
+        private DateTime _dateTaken = DateTime.Now;
+        public DateTime DateTaken
+        {
+            get { return _dateTaken; }
+            set
+            {
+                if (_dateTaken != value)
+                {
+                    _dateTaken = value;
+                    HasChanged = true;
+                }
+            }
+        }
+
+        private string _notes = "";
+        public string Notes
+        {
+            get { return _notes; }
+            set
+            {
+                if (_notes != value)
+                {
+                    _notes = value;
+                    HasChanged = true;
+                }
+            }
+        }
+
+        private bool _hasChanged = true;
+        public bool HasChanged
+        {
+            get { return _hasChanged; }
+            set { _hasChanged = value; }
+        }
+
+        public Photograph(string fileName)
+        {
+            _fileName = fileName;
+            _bitmap = null;
+            _caption = System.IO.Path.GetFileNameWithoutExtension(FileName);
+        }
+        public override bool Equals(object obj)
+        {
+            if (obj is Photograph)
+            {
+                Photograph p = (Photograph)obj;
+                return string.Equals(FileName, p.FileName, StringComparison.InvariantCultureIgnoreCase);
+            }
+            return false;
+        }
+
+        public override int GetHashCode()
+        {
+            return FileName.ToLowerInvariant().GetHashCode();
+        }
+
+        public override string ToString()
+        {
+            return FileName;
+        }
+
+        public void ReleaseImage()
+        {
+            if (_bitmap != null)
+            {
+                _bitmap.Dispose();
+                _bitmap = null;
+            }
+        }
+
+        public void Dispose()
+        {
+            ReleaseImage();
+        }
+    }
+}
